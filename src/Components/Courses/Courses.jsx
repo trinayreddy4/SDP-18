27fd506852bca4d67959b95b28b7c309c0861105
Trinay Reddy <Trinayreddy4@gmail.com>
2023-10-05 20:27:16 +0530

    1.0

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Courses.module.css';
-import Course from '../../Course/Course';
+import Course from '../Course/Course';
 export default function Courses() {
   return (
     <div>
